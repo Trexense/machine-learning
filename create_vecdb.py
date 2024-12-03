b54d290c2df673a408dac1ec4aca482878bf5db0
rayhanadii <rayhan.a.wicaksono@gmail.com>
2024-12-04 00:15:07 +0700

    feat: add hotel data

--- create_vecdb.py
+++ create_vecdb.py
@@ -36,8 +36,8 @@ def load_documents():
 
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=200,
-        chunk_overlap=100,
+        chunk_size=500,
+        chunk_overlap=300,
         length_function=len,
         add_start_index=True,
     )
--- query.py
+++ query.py
@@ -18,6 +18,8 @@ Answer the question based only on the following context:
 
 {context}
 
+Do not Generate Any ID (ID: xxxxx) provided to you into the answer expect there are explicit instruction to do so
+
 ---
 
 Answer the question based on the above context: {question}
@@ -36,15 +38,14 @@ def main():
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.5:
+    results = db.similarity_search_with_relevance_scores(query_text, k=5)
+    if len(results) == 0 or results[0][1] < 0.1:
         print(f"Unable to find matching results.")
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
 
     model = genai.GenerativeModel("gemini-1.5-flash")
     #===
@@ -52,6 +53,7 @@ def main():
 
     # Extract the text content
     response_text = response.candidates[0].content.parts[0].text
+    print(response_text)
     #===
 
 
