e5c01dcfb2516faa3bc3f6f4881d8ba9a6b6cec7
Gerry <150305559+iss4gerry@users.noreply.github.com>
2024-12-10 18:10:53 +0700

    update port

--- run.py
+++ run.py
@@ -2,5 +2,4 @@ import os
 from application import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host="0.0.0.0", port=port)
+      app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
